093a47badee8c1026505c260fb1d7c9148a04331
v.miloserdov <experiencedfame@gmail.com>
2023-01-17 22:34:40 +0100

    add landing and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { LayoutFactoryComponent } from './shared/components/core/layout/layouts/layout-factory/layout-factory.component';
+import { LayoutComponent } from './shared/components/core/layout/layout.component';
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
     component: LayoutFactoryComponent,
-  }
+    loadChildren: () => import('./landing/landing.module').then((m) => m.LandingModule)
+  },
+  // {
+  //   path: 'b2b',
+  //   component: LayoutComponent,
+  //   loadChildren: () => import('./landing/landing.module').then((m) => m.LandingModule),
+  // },
   ]
 
 @NgModule({
---
+++ src/app/landing/landing.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { LandingComponent } from './landing.component';
+import { LandingRoutingModule } from './landing-routing.module';
+
+@NgModule({
+    declarations: [LandingComponent],
+    imports: [LandingRoutingModule],
+    providers: [],
+})
+export class LandingModule {}
